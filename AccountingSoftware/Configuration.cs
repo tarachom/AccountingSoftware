81c4a1d13d86c139527fac805a6a135b18d61810
Tarchom Tarik <tarachom@i.ua>
2019-12-31 21:17:18 +0200

    up

---
+++ AccountingSoftware/Configuration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServerTestErlang.AccountingSoftware
+{
+	//Конфігурація
+	//В цьому класі має міститися вся інформація про конфігурацію
+	class Configuration
+	{
+		string Name { get; set; }
+
+		Dictionary<string, ConfigurationObject> Directories { get; set; }
+
+		Dictionary<string, ConfigurationObject> Documents { get; set; }
+
+		Dictionary<string, ConfigurationObject> Registers { get; set; }
+	}
+}
---
+++ AccountingSoftware/ConfigurationObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServerTestErlang.AccountingSoftware
+{
+	//Клас для обєкту конфігурації
+	//Наприклад довідник чи документ
+	class ConfigurationObject
+	{
+
+	}
+}
