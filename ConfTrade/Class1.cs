af85e93d21caa8f2e920a33cbc81550664f4f0aa
Tarchom Tarik <tarachom@i.ua>
2020-01-01 16:20:36 +0200

    up

--- ConfTrade/Class1.cs
+++ ConfTrade/Class1.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
 
 //Конфігурація Торгівля
 namespace ConfTrade
 {
-    public class Class1
+    public class Configuration
     {
+        public void Load(string path)
+        {
 
+        }
+
+        public void Generation()
+        {
+
+        }
     }
 }
