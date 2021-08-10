e25856e031e04aafb19c2fda903603741f8f89a7
tarachom <tarachom@i.ua>
2021-08-10 18:08:44 +0300

    up

--- Configurator/ConfigurationSelectionForm.Designer.cs
+++ Configurator/ConfigurationSelectionForm.Designer.cs
@@ -28,82 +28,84 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigurationSelectionForm));
-			this.listBoxConfiguration = new System.Windows.Forms.ListBox();
-			this.buttonAddConf = new System.Windows.Forms.Button();
-			this.buttonOpenConf = new System.Windows.Forms.Button();
-			this.buttonDelete = new System.Windows.Forms.Button();
-			this.buttonCopy = new System.Windows.Forms.Button();
-			this.SuspendLayout();
-			// 
-			// listBoxConfiguration
-			// 
-			this.listBoxConfiguration.FormattingEnabled = true;
-			this.listBoxConfiguration.Location = new System.Drawing.Point(12, 12);
-			this.listBoxConfiguration.Name = "listBoxConfiguration";
-			this.listBoxConfiguration.Size = new System.Drawing.Size(502, 277);
-			this.listBoxConfiguration.TabIndex = 0;
-			this.listBoxConfiguration.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBoxConfiguration_MouseDoubleClick);
-			// 
-			// buttonAddConf
-			// 
-			this.buttonAddConf.Location = new System.Drawing.Point(520, 135);
-			this.buttonAddConf.Name = "buttonAddConf";
-			this.buttonAddConf.Size = new System.Drawing.Size(97, 30);
-			this.buttonAddConf.TabIndex = 1;
-			this.buttonAddConf.Text = "Додати";
-			this.buttonAddConf.UseVisualStyleBackColor = true;
-			this.buttonAddConf.Click += new System.EventHandler(this.buttonAddConf_Click);
-			// 
-			// buttonOpenConf
-			// 
-			this.buttonOpenConf.Location = new System.Drawing.Point(520, 12);
-			this.buttonOpenConf.Name = "buttonOpenConf";
-			this.buttonOpenConf.Size = new System.Drawing.Size(148, 30);
-			this.buttonOpenConf.TabIndex = 3;
-			this.buttonOpenConf.Text = "Відкрити конфігуратор";
-			this.buttonOpenConf.UseVisualStyleBackColor = true;
-			this.buttonOpenConf.Click += new System.EventHandler(this.buttonOpenConf_Click);
-			// 
-			// buttonDelete
-			// 
-			this.buttonDelete.Location = new System.Drawing.Point(520, 258);
-			this.buttonDelete.Name = "buttonDelete";
-			this.buttonDelete.Size = new System.Drawing.Size(97, 30);
-			this.buttonDelete.TabIndex = 4;
-			this.buttonDelete.Text = "Видалити";
-			this.buttonDelete.UseVisualStyleBackColor = true;
-			this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
-			// 
-			// buttonCopy
-			// 
-			this.buttonCopy.Location = new System.Drawing.Point(520, 171);
-			this.buttonCopy.Name = "buttonCopy";
-			this.buttonCopy.Size = new System.Drawing.Size(97, 30);
-			this.buttonCopy.TabIndex = 5;
-			this.buttonCopy.Text = "Копіювати";
-			this.buttonCopy.UseVisualStyleBackColor = true;
-			this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
-			// 
-			// ConfigurationSelectionForm
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(675, 300);
-			this.Controls.Add(this.buttonCopy);
-			this.Controls.Add(this.buttonDelete);
-			this.Controls.Add(this.buttonOpenConf);
-			this.Controls.Add(this.buttonAddConf);
-			this.Controls.Add(this.listBoxConfiguration);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "ConfigurationSelectionForm";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "Вибір конфігурації";
-			this.Load += new System.EventHandler(this.ConfigurationSelectionForm_Load);
-			this.ResumeLayout(false);
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ConfigurationSelectionForm));
+            this.listBoxConfiguration = new System.Windows.Forms.ListBox();
+            this.buttonAddConf = new System.Windows.Forms.Button();
+            this.buttonOpenConf = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxConfiguration
+            // 
+            this.listBoxConfiguration.Font = new System.Drawing.Font("Consolas", 12F);
+            this.listBoxConfiguration.FormattingEnabled = true;
+            this.listBoxConfiguration.ItemHeight = 19;
+            this.listBoxConfiguration.Location = new System.Drawing.Point(12, 12);
+            this.listBoxConfiguration.Name = "listBoxConfiguration";
+            this.listBoxConfiguration.Size = new System.Drawing.Size(502, 270);
+            this.listBoxConfiguration.TabIndex = 0;
+            this.listBoxConfiguration.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBoxConfiguration_MouseDoubleClick);
+            // 
+            // buttonAddConf
+            // 
+            this.buttonAddConf.Location = new System.Drawing.Point(520, 135);
+            this.buttonAddConf.Name = "buttonAddConf";
+            this.buttonAddConf.Size = new System.Drawing.Size(97, 30);
+            this.buttonAddConf.TabIndex = 1;
+            this.buttonAddConf.Text = "Додати";
+            this.buttonAddConf.UseVisualStyleBackColor = true;
+            this.buttonAddConf.Click += new System.EventHandler(this.buttonAddConf_Click);
+            // 
+            // buttonOpenConf
+            // 
+            this.buttonOpenConf.Location = new System.Drawing.Point(520, 12);
+            this.buttonOpenConf.Name = "buttonOpenConf";
+            this.buttonOpenConf.Size = new System.Drawing.Size(148, 30);
+            this.buttonOpenConf.TabIndex = 3;
+            this.buttonOpenConf.Text = "Відкрити конфігуратор";
+            this.buttonOpenConf.UseVisualStyleBackColor = true;
+            this.buttonOpenConf.Click += new System.EventHandler(this.buttonOpenConf_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(520, 258);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(97, 30);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "Видалити";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonCopy
+            // 
+            this.buttonCopy.Location = new System.Drawing.Point(520, 171);
+            this.buttonCopy.Name = "buttonCopy";
+            this.buttonCopy.Size = new System.Drawing.Size(97, 30);
+            this.buttonCopy.TabIndex = 5;
+            this.buttonCopy.Text = "Копіювати";
+            this.buttonCopy.UseVisualStyleBackColor = true;
+            this.buttonCopy.Click += new System.EventHandler(this.buttonCopy_Click);
+            // 
+            // ConfigurationSelectionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 300);
+            this.Controls.Add(this.buttonCopy);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonOpenConf);
+            this.Controls.Add(this.buttonAddConf);
+            this.Controls.Add(this.listBoxConfiguration);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ConfigurationSelectionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Вибір конфігурації";
+            this.Load += new System.EventHandler(this.ConfigurationSelectionForm_Load);
+            this.ResumeLayout(false);
 
 		}
 
